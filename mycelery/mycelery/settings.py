b176b1a2ab1d89171a0a6ff53012f076c41b37cf
taehun33.kim <taehun33.kim@samsung.com>
2020-07-15 13:33:16 +0900

    Update settings

--- mycelery/mycelery/settings.py
+++ mycelery/mycelery/settings.py
@@ -114,9 +114,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CELERY_BROKER_URL = 'amqp://rakib:123456@localhost:5672/rakibhost'
-CELERY_RESULT_BACKEND = 'amqp://rakib:123456@localhost:5672/rakibhost'
+CELERY_BROKER_URL = 'amqp://admin:mypass@localhost:5672'
+CELERY_RESULT_BACKEND = 'amqp://admin:mypass@localhost:5672'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-
